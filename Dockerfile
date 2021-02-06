48cb6de8541e3f2c7a67cb5f85c4c8232a03b791
xgao <gao.gaox@gmail.com>
2021-02-06 14:38:58 -0500

    run by main

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ COPY ./app /app
 
 WORKDIR /app
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["python", "main.py"]
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,3 +8,5 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
